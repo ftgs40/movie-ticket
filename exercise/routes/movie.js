52b5f4ff37894435eaee33135aa13bcb4592ab23
Nakarin <ftgs40@gmail.com>
2018-08-15 07:50:46 +0700

    add date sort route

--- exercise/routes/movie.js
+++ exercise/routes/movie.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../database');
+const movieCollect = db.get('movies');
 
-router.get('/', (req, res) =>{
+router.get('/time', (req, res) => {
+    // res.send('movie List')
+    movieCollect.find()
+        .then(movieList => {
+            res.json(movieList);
+        });
+})
+
+router.get('/date/:sort*?', (req, res) => {
+
+    let sort = {}
+    if(typeof req.params.sort != "undefined"){
+        sort = {sort: {name: parseInt(req.params.sort)}}
+    }
+    movieCollect.find({}, sort)
+        .then(movieList => {
+            res.json(movieList);
+        });
+})
+
+router.get('/price', (req, res) => {
     res.send('movie List')
+})
 
+router.get('/search/:name', (req, res) => {
+    res.send('movie List')
 })
 
 module.exports = router;
